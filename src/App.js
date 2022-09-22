ce715cca424bca584622f2ed1d16ebe80ca0432b
athamov <abdulloxathamoff@gmail.com>
2022-09-22 16:21:44 +0500

    style changed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,3 @@
-// import './App.css';
-// import { createRestaurant, deleteRestaurant} from './graphql/mutations'
-// import { listRestaurants } from './graphql/queries'
-// import { withAuthenticator, Button, Text, Flex, Heading, TextField  } from "@aws-amplify/ui-react";
-// import { useCallback, useEffect, useState } from 'react';
-// import { API } from 'aws-amplify';
-
-// function App({ signOut,user }) {
-//   const [name,setName] = useState("name");
-//   const [description,setDescription] = useState("description");
-//   const [ address,setAddress] = useState("address");
-//   const [ restaurants, setRestaurants ] = useState([]);
-
-//   const fetchRestaurants = useCallback(async () => {
-//     const result = await API.graphql({
-//       query: listRestaurants,
-//       authMode: 'AMAZON_COGNITO_USER_POOLS'
-//     })
-//     setRestaurants(result.data.listRestaurants.items)
-//   }, [setRestaurants]);
-
-//   const handleCreateRestaurant = useCallback(async () => {
-//     await API.graphql({
-//       query: createRestaurant,
-//       variables: { input: {name:name,description:description,location:address} },
-//       authMode: 'AMAZON_COGNITO_USER_POOLS'
-//     })
-//     fetchRestaurants()
-//   }, [fetchRestaurants,name,address,description]);
-
-//   const handleDeleteRestaurant = useCallback(async (id) => {
-//     await API.graphql({
-//       query: deleteRestaurant,
-//       variables: { input: { id: id } },
-//       authMode: 'AMAZON_COGNITO_USER_POOLS'
-//     })
-//     fetchRestaurants()
-//   }, [fetchRestaurants]);
-
-//   useEffect(() => {
-//     fetchRestaurants()
-//   }, [fetchRestaurants]);
-
-//   return (
-//     <Flex direction={"column"}>
-//     </Flex>
-//   );
-// };
-
-// export default withAuthenticator(App);
-
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { API } from 'aws-amplify';
 import { useEffect, useState } from 'react';
@@ -86,8 +35,35 @@ function App({ signOut,user }) {
 
   return (
     <div className="App text-bg-secondary text-info">
-      <h1 className="text-primary">My Restaurants App</h1>
+      <div className="row">
+        <h1 className="text-primary col-9">My Restaurants App</h1>
+        <button onClick={signOut} className="btn btn-warning text-light col-3">Sign Out</button>
+      </div>
       <h3>{user.userName}</h3>
+
+      <table className="table table-primary table-striped table-striped-white text-success">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Location</th>
+            <th scope="col">#</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Restaurants.map(Restaurant => (
+              <tr key={Restaurant.id || Restaurant.name}>
+                <th scope="col">{Restaurant.name}</th>
+                <td>{Restaurant.description}</td>
+                <td>{Restaurant.location}</td>
+                <td><button className="btn btn-danger" onClick={() => DeleteRestaurant(Restaurant)}>Delete</button></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
       <div className="container text-center">
         <div className="row">
           <div className="col input-group flex-nowrap">
@@ -129,29 +105,7 @@ function App({ signOut,user }) {
         </div>
       </div>
       <button onClick={CreateRestaurant} className="btn btn-info text-light m-3">Create Restaurant</button>
-      <table className="table table-primary table-striped table-striped-white text-success">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Location</th>
-            <th scope="col">#</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Restaurants.map(Restaurant => (
-              <tr key={Restaurant.id || Restaurant.name}>
-                <th scope="col">{Restaurant.name}</th>
-                <td>{Restaurant.description}</td>
-                <td>{Restaurant.location}</td>
-                <td><button className="btn btn-danger" onClick={() => DeleteRestaurant(Restaurant)}>Delete</button></td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
-      <button onClick={signOut} className="btn btn-warning text-light">Sign Out</button>
+
     </div>
   );
 }
